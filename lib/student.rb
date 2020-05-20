5fb26340b81e23e7610a53d5b2ea0afdf3d0240f
adeeba reaz <adeebarivera@outlook.com>
2020-05-20 18:43:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,12 @@
-class Student 
- def 
+class Student < User
+  attr_accessor :knowledge
 
+
+end 	  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    knowledge << string
+  end
 end
